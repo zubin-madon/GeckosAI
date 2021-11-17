f62d19cc97132bd5a513481d4c6d25cb27221c9f
Zubin Madon <35947376+zubin-madon@users.noreply.github.com>
2021-11-17 09:15:51 +0530

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -333,7 +333,7 @@ const Home = (props: HomeProps) => {
 
           <button
             className="bg-black border-white rounded-none flex font-semibold border-2 mt-12 text-white py-3 px-10 top-5 absolute items-center hover:(text-black bg-white border-black) "
-            onClick={() => setModalOpen(true)}
+            onClick={() => setModalOpen(false)}
           >
             MINT
           </button>
